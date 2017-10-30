3d5aa3f446e861551c152e3382115fe1aa2ef837
Bruno Pereira <2151219@my.ipleiria.pt>
2017-10-30 12:28:56 +0000

    fix toolbar

--- app/src/main/java/com/example/bruno/iair/CityListActivity.java
+++ app/src/main/java/com/example/bruno/iair/CityListActivity.java
@@ -3,12 +3,18 @@ package com.example.bruno.iair;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+
+import static java.lang.Boolean.FALSE;
 
 public class CityListActivity extends AppCompatActivity {
     private ListView listOfCities;
@@ -18,6 +24,10 @@ public class CityListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_list);
+
+        //        MAIN_MENU
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         Spinner spinner = findViewById(R.id.countrySpinner);
         final String[] countrys = {"Portugal","Spain","France","USA"};
@@ -38,6 +48,15 @@ public class CityListActivity extends AppCompatActivity {
                 startActivity(appInfo);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_itens, menu);
+        MenuItem item = menu.findItem(R.id.btnCity);
+        item.setVisible(false);
+        return true;
     }
 
 }
