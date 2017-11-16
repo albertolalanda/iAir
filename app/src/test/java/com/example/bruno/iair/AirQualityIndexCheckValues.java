e78f3d8bfece2bb785f84a3c6abf743d5190979c
lalanda <lalanda.alberto12@gmail.com>
2017-11-16 14:05:44 +0000

    TAES2017G7-37

--- app/src/test/java/com/example/bruno/iair/AirQualityIndexCheckValues.java
+++ app/src/test/java/com/example/bruno/iair/AirQualityIndexCheckValues.java
@@ -1,5 +1,8 @@
 package com.example.bruno.iair;
 
+import com.example.bruno.iair.models.City;
+import com.example.bruno.iair.models.Country;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,7 +14,21 @@ import static org.junit.Assert.assertEquals;
  */
 public class AirQualityIndexCheckValues {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void gets_data() throws Exception {
+        City city = new City("Leiria", new Country("Portugal","PT"), 39.7495331, -8.807683, 30.0, 18.43, 50, 50, 50);
+        City city2 = new City("Leiria", new Country("Portugal","PT"), 39.7495331, -8.807683, 30.0, 18.43, 100, 100, 100);
+        City city3 = new City("Leiria", new Country("Portugal","PT"), 39.7495331, -8.807683, 30.0, 18.43, 200, 200, 200);
+        City city4 = new City("Leiria", new Country("Portugal","PT"), 39.7495331, -8.807683, 30.0, 18.43, 300, 300, 300);
+        City city5 = new City("Leiria", new Country("Portugal","PT"), 39.7495331, -8.807683, 30.0, 18.43, 400, 400, 400);
+        City city6 = new City("Leiria", new Country("Portugal","PT"), 39.7495331, -8.807683, 30.0, 18.43, 500, 500, 500);
+
+
+        assertEquals("Good", city.getAQI());
+        assertEquals("Moderate", city2.getAQI());
+        assertEquals("Unhealthy for Sensitive Groups", city3.getAQI());
+        assertEquals("Unhealthy", city4.getAQI());
+        assertEquals("Very Unhealthy", city5.getAQI());
+        assertEquals("Hazardous", city6.getAQI());
+
     }
 }
