4524962d9f045e092c64e2eb4bd19df457bfb4e2
lalanda <lalanda.alberto12@gmail.com>
2017-11-29 22:50:32 +0000

    TAES2017G7-81

---
+++ app/src/androidTest/java/com/example/bruno/iair/activities/AddRecentEventToThinkSpeak.java
@@ -0,0 +1,124 @@
+package com.example.bruno.iair.activities;
+
+
+import android.support.test.espresso.DataInteraction;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.example.bruno.iair.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class AddRecentEventToThinkSpeak {
+
+    @Rule
+    public ActivityTestRule<DashBoardActivity> mActivityTestRule = new ActivityTestRule<>(DashBoardActivity.class);
+
+    @Test
+    public void addRecentEventToThinkSpeak() {
+        DataInteraction linearLayout = onData(anything())
+                .inAdapterView(allOf(withId(R.id.cityList),
+                        childAtPosition(
+                                withClassName(is("android.widget.LinearLayout")),
+                                1)))
+                .atPosition(3);
+        linearLayout.perform(click());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.sendEventsBtn), withText("Send Events"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.layoutInfoo),
+                                        7),
+                                0),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction linearLayout2 = onView(
+                allOf(withId(R.id.layoutEarthquake),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.radioEvent),
+                                        0),
+                                2),
+                        isDisplayed()));
+        linearLayout2.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.sendBtn), withText("Send"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                2),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.btnBack), withContentDescription("Item"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.main_toolbar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(android.R.id.text1), withText("29/11/2017 22:47 | Earthquake | user-1342201123"),
+                        childAtPosition(
+                                allOf(withId(R.id.LVEventList),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(isDisplayed()));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
