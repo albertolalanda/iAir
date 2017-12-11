541d4d4857f150920dd14403dc96d959cef17976
Filipe Henriques <2151211@my.ipleiria.pt>
2017-12-11 14:34:53 +0000

    TAES2017G7-99

---
+++ app/src/test/java/com/example/bruno/iair/AirQualityIndexValueColorsTest.java
@@ -0,0 +1,35 @@
+package com.example.bruno.iair;
+
+import com.example.bruno.iair.models.City;
+import com.example.bruno.iair.models.Country;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class AirQualityIndexValueColorsTest {
+    @Test
+    public void gets_data() throws Exception {
+        //TODO: this
+
+        City city = new City("Leiria", new Country("Portugal","PT"), 39.7495331, -8.807683);
+        City city2 = new City("Leiria", new Country("Portugal","PT"), 39.7495331, -8.807683);
+        City city3 = new City("Leiria", new Country("Portugal","PT"), 39.7495331, -8.807683);
+        City city4 = new City("Leiria", new Country("Portugal","PT"), 39.7495331, -8.807683);
+        City city5 = new City("Leiria", new Country("Portugal","PT"), 39.7495331, -8.807683);
+        City city6 = new City("Leiria", new Country("Portugal","PT"), 39.7495331, -8.807683);
+
+        assertEquals("Green", city.getAQI());
+        assertEquals("Yellow", city2.getAQI());
+        assertEquals("Orange", city3.getAQI());
+        assertEquals("Light red", city4.getAQI());
+        assertEquals("Purple", city5.getAQI());
+        assertEquals("Dark red", city6.getAQI());
+
+    }
+}
