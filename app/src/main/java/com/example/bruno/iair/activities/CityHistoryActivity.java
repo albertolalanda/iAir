1cadb7a12edcdbf111da5deb4151d1f012fb77ce
Tiago Martins <2151237@my.ipleiria.pt>
2017-12-13 11:42:24 +0000

    TAES2017G7-103

---
+++ app/src/main/java/com/example/bruno/iair/activities/CityHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.bruno.iair.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.bruno.iair.R;
+
+public class CityHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_history);
+    }
+}
